8109f35ef57baed5501c6d1c74834267a529f2ca
Ben <ben@nyx.(none)>
2015-01-08 17:12:50 -0500

    more specificity

--- tweet_to_codes.py
+++ tweet_to_codes.py
@@ -3,17 +3,29 @@ import numpy as np
 
 codes = np.genfromtxt('just_the_codes.txt', dtype=str)
 
-print codes
+#print codes
 
 
 def tweet_to_codes(tweet):
     out = []
-    words = re.findall(r"[\w']+", tweet)
+  #  words = re.findall(r"[\w']+", tweet)
+    words1 = re.findall(r"([a-zA-Z0-9]*->[a-zA-Z0-9]*)", tweet)
+  #  words2 = re.findall(r"([a-zA-Z0-9]*-[a-zA-Z0-9]*)", tweet)
 
-    for i in xrange(1, len(words)):
-        if (words[i-1] in codes) and (words[i] in codes):
-            out.append(words[i-1])
-            out.append(words[i])
+   
+    for i in xrange(len(words1)):
+        words_sh = words1[i].split('->')
+     #   print words_sh
+        for j in xrange(1, len(words_sh)):
+            if (words_sh[i-1] in codes) and (words_sh[i] in codes):
+                out.append(words_sh[i-1])
+                out.append(words_sh[i])
+            
+
+  #  for i in xrange(1, len(words)):
+  #      if (words[i-1] in codes) and (words[i] in codes):
+  #          out.append(words[i-1])
+  #          out.append(words[i])
 
 #    print h
 
@@ -21,6 +33,6 @@ def tweet_to_codes(tweet):
 
 if __name__ == "__main__":
 
-    out = tweet_to_codes('this is an example of an LHR->MDW tweet #ORD #BOS sea-lax')
+    out = tweet_to_codes('this is an example of an LHR->MDW tweet #ORD #BOS SEA-LAX')
 
     print out
