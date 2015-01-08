d02893e68ac5531750e10ec502456587e84e41e4
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-01-08 13:27:37 -0800

    silly Ben

---
+++ tweet_to_codes.py
@@ -0,0 +1,25 @@
+import re
+import numpy as np
+
+codes = np.genfromtxt('just_the_codes.txt', dtype=str)
+
+print codes
+
+out = []
+
+def tweet_to_codes(tweet):
+    words = re.findall(r"[\w']+", tweet)
+
+    for i in words:
+        if i in codes:
+            out.append(i)
+
+#    print h
+
+    return out
+
+if __name__ == "__main__":
+
+    tweet_to_codes('this is an example of a tweet #ORD #BOS sea-lax')
+
+    print out
