597710c0c53322c1f57c5b3f78d093a615cddc23
Ben <ben@nyx.(none)>
2015-01-08 17:26:52 -0500

    more flexibility, allow -> or -

--- tweet_to_codes.py
+++ tweet_to_codes.py
@@ -9,13 +9,14 @@ codes = np.genfromtxt('just_the_codes.txt', dtype=str)
 def tweet_to_codes(tweet):
     out = []
   #  words = re.findall(r"[\w']+", tweet)
-    words1 = re.findall(r"([a-zA-Z0-9]*->[a-zA-Z0-9]*)", tweet)
-  #  words2 = re.findall(r"([a-zA-Z0-9]*-[a-zA-Z0-9]*)", tweet)
+    words1 = re.findall(r"([a-zA-Z0-9]{3}->[a-zA-Z0-9]{3})", tweet)
+    words2 = re.findall(r"([a-zA-Z0-9]{3}-[a-zA-Z0-9]{3})", tweet)
 
+    words = words1 + words2
    
-    for i in xrange(len(words1)):
-        words_sh = words1[i].split('->')
-     #   print words_sh
+    for i in xrange(len(words)):
+        words_sh = words[i].replace('>','').split('-')
+        print words_sh
         for j in xrange(1, len(words_sh)):
             if (words_sh[i-1] in codes) and (words_sh[i] in codes):
                 out.append(words_sh[i-1])
