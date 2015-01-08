6e466e58eb6691cdf0c1881f5ea5bcc843d7497c
Ben <ben@nyx.(none)>
2015-01-08 17:47:54 -0500

    spaces in -> term allowed

--- tweet_to_codes.py
+++ tweet_to_codes.py
@@ -9,14 +9,17 @@ codes = np.genfromtxt('just_the_codes.txt', dtype=str)
 def tweet_to_codes(tweet):
     out = []
   #  words = re.findall(r"[\w']+", tweet)
-    words1 = re.findall(r"([a-zA-Z0-9]{3}->[a-zA-Z0-9]{3})", tweet)
-    words2 = re.findall(r"([a-zA-Z0-9]{3}-[a-zA-Z0-9]{3})", tweet)
+    tweetcond = tweet.replace(' -> ', '->')
+    tweetcond = tweetcond.replace(' - ', '-')
+
+    words1 = re.findall(r"([a-zA-Z]{3}->[a-zA-Z]{3})", tweetcond)
+    words2 = re.findall(r"([a-zA-Z]{3}-[a-zA-Z]{3})", tweetcond) 
 
     words = words1 + words2
    
     for i in xrange(len(words)):
         words_sh = words[i].replace('>','').split('-')
-     #   print words_sh
+  
         for j in xrange(1, len(words_sh)):
             if (words_sh[i-1] in codes) and (words_sh[i] in codes):
                 out.append(words_sh[i-1])
@@ -34,6 +37,6 @@ def tweet_to_codes(tweet):
 
 if __name__ == "__main__":
 
-    out = tweet_to_codes('this is an example of an LHR->MDW tweet #ORD #BOS SEA-LAX')
+    out = tweet_to_codes('this is an example of an LHR -> MDW tweet #ORD #BOS SEA-LAX')
 
     print out
